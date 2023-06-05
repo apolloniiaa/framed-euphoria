fbb7754eacd63809e42bf8fc2783c3a18db8764d
apolloniiaa <apoll93@hotmail.com>
2023-06-05 13:34:48 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,42 +8,39 @@ import style from './Footer.module.css';
 const Footer = () => {
   return (
     <div>
+      <div className={style.footerIcons}>
+        <a
+          href='https://www.instagram.com/balogh.janos.photoblog/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaInstagram size={30} />
+        </a>
+        <a
+          href='https://www.facebook.com/vonburied.john'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaFacebook size={30} />
+        </a>
+        <a
+          href='mailto:balogh.janos1011@gmail.com'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaEnvelope size={30} />
+        </a>
+
+        <a
+          href='https://www.linkedin.com/in/janos-balogh-83a21521b/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaLinkedin size={30} />
+        </a>
+      </div>
+
       <div className={style.footer}>
-        <div className={style.footerRow}>
-          <div className={style.footerContentCenter}></div>
-          <div className={style.footerContentRight}>
-            {/* <div className={style.footerIcons}>
-              <a
-                href='https://www.instagram.com/your-instagram-account'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <FaInstagram size={30} />
-              </a>
-              <a
-                href='https://www.facebook.com/your-facebook-account'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <FaFacebook size={30} />
-              </a> */}
-            {/* <a
-                href='https://www.facebook.com/your-facebook-account'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <FaEnvelope size={30} />
-              </a>
-              <a
-                href='https://www.linkedin.com/your-linkedin-account'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <FaLinkedin size={30} />
-              </a> */}
-            {/* </div> */}
-          </div>
-        </div>
         <div className={style.footerRow}>
           <div className={style.footerCopyright}>
             &copy; 2023 Woodland Capture. All rights reserved.
