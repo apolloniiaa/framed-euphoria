92b3b90d6a68e3e29531ac7440d71afb742a1fa1
apolloniiaa <apoll93@hotmail.com>
2023-07-31 17:01:41 +0200

    update content

--- src/movieState.js
+++ src/movieState.js
@@ -4,14 +4,19 @@ import img2 from './assets/img/img44-md.jpg';
 import img3 from './assets/img/img11-md.jpg';
 import img4 from './assets/img/img28-md.jpg';
 import img5 from './assets/img/img48-md.jpg';
+import img6 from './assets/img/img38-md.jpg';
 import img26 from './assets/img/paros-banner.jpg';
+import img27 from './assets/img/img9-md.jpg';
 
 //jegyes
 import img9 from './assets/img/img8-md.jpg';
 import img10 from './assets/img/img29-md.jpg';
 import img11 from './assets/img/img4-md.jpg';
+import img12 from './assets/img/img6-md.jpg';
+import img13 from './assets/img/img5-md.jpg';
 
 //eskuvo
+import img30 from './assets/img/14.jpg';
 import img17 from './assets/img/img12-md.jpg';
 import img18 from './assets/img/img27-md.jpg';
 import img19 from './assets/img/img3-md.jpg';
@@ -21,6 +26,9 @@ import img22 from './assets/img/img40-md.jpg';
 import img23 from './assets/img/img23-md.jpg';
 import img24 from './assets/img/img24-md.jpg';
 import img25 from './assets/img/img15-md.jpg';
+import img28 from './assets/img/img15-md.jpg';
+import img29 from './assets/img/img36-md.jpg';
+import img31 from './assets/img/img20-md.jpg';
 
 export const MovieState = () => {
   const galleryImages = [
@@ -29,24 +37,32 @@ export const MovieState = () => {
     { id: 3, imgSrc: img3 },
     { id: 4, imgSrc: img4 },
     { id: 5, imgSrc: img5 },
+    { id: 6, imgSrc: img6 },
+    { id: 9, imgSrc: img27 },
   ];
 
   const galleryImages2 = [
     { id: 9, imgSrc: img9 },
     { id: 10, imgSrc: img10 },
     { id: 11, imgSrc: img11 },
+    { id: 12, imgSrc: img12 },
+    { id: 13, imgSrc: img13 },
   ];
 
   const galleryImages3 = [
+    { id: 30, imgSrc: img30 },
     { id: 17, imgSrc: img17 },
     { id: 18, imgSrc: img18 },
     { id: 19, imgSrc: img19 },
     { id: 20, imgSrc: img20 },
     { id: 21, imgSrc: img21 },
     { id: 22, imgSrc: img22 },
+    { id: 31, imgSrc: img31 },
     { id: 23, imgSrc: img23 },
     { id: 24, imgSrc: img24 },
     { id: 25, imgSrc: img25 },
+    { id: 28, imgSrc: img28 },
+    { id: 29, imgSrc: img29 },
   ];
 
   return [
